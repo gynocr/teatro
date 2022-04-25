1f069368e009e0291d0754013a234f9cca70957b
Geovanny AS <87682163+YonRasgg@users.noreply.github.com>
2022-04-25 17:18:30 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class App {
+
+    final static scanner = new Scanner(System.in);
+    static Teatro teatro;
     public static void main(String[] args) throws Exception {
-        Teatro teatro = new Teatro(11, 12);
+        String menu = "\n1.Ver asientos disponibles\n2.Comprar boletos \n3.Estadisticas de la sala \n4.Salir\n";
+        System.out.println("Se ha creado una sala de teatro favor ingresar: \n");
+        System.out.println("Ingresar filas");
+        int filas = scanner.nextInt();
+        System.out.println("Ingresar columnas");
+        int columnas = scanner.nextInt();
+        teatro = new Teatro(filas, columnas);
         teatro.llenarSala();
         teatro.mostrarSala();
     }
