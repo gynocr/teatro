3a85c0654e2f72daabd7ccfe6328e2089617b40f
Geovanny AS <87682163+YonRasgg@users.noreply.github.com>
2022-04-25 16:53:34 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Teatro teatro = new Teatro(11, 12);
+        teatro.llenarSala();
+        teatro.mostrarSala();
+    }
+}
